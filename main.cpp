16b4265a91cdec6dfe290141c591bd9bb4efe716
cmgruner <68166683+cmgruner@users.noreply.github.com>
2025-02-12 13:47:09 -0700

    Backstory and Typing Effect
    Had ChatGPT write a backstory for an adventure game.  Put the text into a backstory.txt file.  Found code that would generate a typing effect for printing out the backstory (the function printWithTypingEffect).  Wrote the code in main to open the file and print out the backstory.  It is buggy and prints out the last sentence twice...

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <thread>
 using namespace std;
+
+void printWithTypingEffect(const string &text, int speed_ms = 50) 
+{
+    for (char c : text) {
+        cout << c << flush;
+        this_thread::sleep_for(chrono::milliseconds(speed_ms));
+    }
+    cout << endl;
+}
 
 int main()
 {
-    cout << "This is a choose your own adventure game!" << endl;
-    
-    // TODO: Write a backstory
-    
-    // TODO:  Add questions and decision structures
+    // Read in the backstory.txt and write it out to the screen
+    ifstream inputFile;
+    inputFile.open("backstory.txt");
+    string line;
+    do
+    {
+        getline(inputFile, line);
+        printWithTypingEffect(line, 2);
+    } while(inputFile); 
+    inputFile.close();  
+
+    // Write the rest of the game here......
 }
