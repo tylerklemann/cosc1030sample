4a1b161c6bfabecf32088a6d03ecbe7c4d23c5a3
Charlotte Gruner <60362002+charlottegruner@users.noreply.github.com>
2025-02-12 11:01:22 -0700

    Add files via upload
    The start of a story adventure game.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "This is a choose your own adventure game!" << endl;
+    
+    // TODO: Write a backstory
+    
+    // TODO:  Add questions and decision structures
+}
